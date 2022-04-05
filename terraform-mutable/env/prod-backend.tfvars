0cd117e4b55dd970408dd6052d872564746c9632
vasu <bandaruvasu1@gmail.com>
2022-04-05 20:33:04 +0530

    frontend

---
+++ terraform-mutable/env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "vasudevops"
+key    = "terraform-mutable/user/frontend/terraform.tfstate"
+region = "us-east-1"
---
+++ terraform-mutable/main.tf
@@ -0,0 +1,16 @@
+module "frontend" {
+  source = "git::https://github.com/vasubandaru1/terraform-mutable.git//app-module"
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  ENV   = var.ENV
+  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
+  COMPONENT           = "frontend"
+  PORT                = 8080
+  IS_PRIVATE_LB        = false
+  LB_RULE_PRIORITY     = 102
+
+}
+
+output "INSTANCE_IDS" {
+  value = module.frontend.INSTANCE_IDS
+}
